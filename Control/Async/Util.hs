559c80e42eec35bf7d9e86126feebef49623e216
Bill Casarin <bill@casarin.ca>
2011-07-26 06:41:52 -0700

    Function for lifting IO functions into a safer environment

--- Control/Async/Util.hs
+++ Control/Async/Util.hs
@@ -2,12 +2,15 @@
 
 module Control.Async.Util ( mapA
                           , mapAS
+                          , liftS
+                          , liftST
                           , AsyncError(..)
                           ) where
   
 import Control.Async (waitForAsync, forkAsync, Async(..))
 import System.Timeout (timeout)
 import Control.Exception
+import Control.Monad
 
 data AsyncError = Timeout
                 | Exception SomeException
@@ -22,19 +25,38 @@ async = forkAsync
 wait :: Async a -> IO a
 wait = waitForAsync
 
+
+mapAsync :: (a -> IO b) -> [a] -> IO [Async b]
+mapAsync f xs = mapM (async . f) xs
+
 -- | Map an IO-producing function over a list, executing each one asyncronously.
 --   Timeouts and exceptions are not handled by this function, so use it
 --   carefully
 mapA :: (a -> IO b) -> [a] -> IO [b]
-mapA f xs = mapM (async . f) xs >>= mapM wait
+mapA f xs = mapAsync f xs >>= mapM wait
+
+
+mapA_ :: (a -> IO b) -> [a] -> IO ()
+mapA_ f xs = mapAsync f xs >>= mapM_ wait
+
 
 -- | Very safe version of mapA. The first argument is the timeout in
 --   microseconds. Any timeouts or exceptions are returned as a Left constructor
 --   holding the error that occured. Successful results are held in the Right
---   constructor
+--   constructor.
 mapAS :: Int -> (a -> IO b) -> [a] -> IO [Either AsyncError b]
-mapAS n f xs = mapA (timeout n . try . f) xs >>= return . map conv
+mapAS n f xs = mapA (liftST n f) xs
+
+
+liftS :: (a -> IO b) -> a -> IO (Either SomeException b)
+liftS f = try . f
+
+
+liftST :: Int -> (a -> IO b) -> a -> IO (Either AsyncError b)
+liftST n f = liftM conv . try . timeout n . f
   where
-    conv Nothing                          = Left Timeout
-    conv (Just (Left (e::SomeException))) = Left (Exception e)
-    conv (Just (Right r))                 = Right r
+    conv :: Either SomeException (Maybe a) -> Either AsyncError a
+    conv (Left e)         = Left (Exception e)
+    conv (Right Nothing)  = Left Timeout
+    conv (Right (Just r)) = Right r
+
